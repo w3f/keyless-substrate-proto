24488d3ef906b5692e0046ba3e95696d739616c3
Andrew Burger <andrewburger1991@gmail.com>
2024-10-11 07:11:20 +0200

    adding todos and initial things

--- prover_service/src/main.rs
+++ prover_service/src/main.rs
@@ -1,5 +1,59 @@
 use aptos_keyless_common::api::{RequestInput, ProverServiceResponse};
 
+
+pub type Bytes = Vec<u8>;
+#[derive(Default)]
+pub struct Groth16Vk {
+    pub alpha_g1: Bytes,
+    pub beta_g1: Bytes,
+    pub delta_g2: Bytes,
+    pub gamma_abc_g1: Vec<Bytes>,
+    pub gamma_g2: Bytes,
+}
+
+// TODO: Implement
+#[derive(Default)]
+pub struct RapidProver {}
+#[derive(Default)]
+// TODO: Implement
+pub struct Config {}
+#[derive(Default)]
+// TODO: Implement
+pub struct CircuitConfig {}
+
+#[derive(Default)]
+pub struct State {
+    prover: Mutex<RapidProver>, // from rust-snark wrapper
+    groth16_vk: Groth16VK,
+    config: Config,
+    circuit_config: CircuitConfig,
+}
+
+impl State {
+    fn new() -> Self {
+        Default::default()
+    }
+}
+
+// TODO: Make main Tokio runtime
 fn main() {
-    println!("Hello, world!");
+    // TODO: Add Service to do some sending/receiving of proof requests etc
+
+
+    //TODO: log service
+
+    //TODO: Add verification key file read
+
+    //TODO: Initialisze State
+    //TODO: Config
+    //TODO: wrap state in arc pointer to share to threads
+
+    //TODO: Fetch JWK refresh in some interval -- async fn
+
+    // Some metrics service start perhaps
+}
+
+#[test]
+fn can_create_state() {
+    assert!(State::new())
 }
